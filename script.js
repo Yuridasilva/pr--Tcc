4e3a07c5277ce2c03e535f8d26996c158a450471
Yuridasilva <yuridaasilva@gmail.com>
2024-11-11 22:24:32 -0300

    retirado o foto

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Obtém o botão de alternância
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    // Verifica se o modo escuro foi previamente ativado
+    const currentMode = localStorage.getItem('mode');
+    if (currentMode === 'dark') {
+        document.body.classList.add('dark-mode');
+        toggleButton.innerHTML = '🌞';  // Altera o ícone do botão para "sol"
+    }
+
+    // Adiciona o evento de clique para alternar os modos
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('mode', 'dark');
+            toggleButton.innerHTML = '🌞';  // Altera o ícone para "sol"
+        } else {
+            localStorage.setItem('mode', 'light');
+            toggleButton.innerHTML = '🌙';  // Altera o ícone para "lua"
+        }
+    });
+
 let radio = document.querySelector('.manual-btn')
 let cont = 1
 
@@ -5,12 +27,12 @@ document.getElementById('radio1').checked = true
 
 setInterval(() => {
     proximaImg()
-}, 5000)
+}, 3000)
 
 function proximaImg(){
     cont++
 
-    if(cont > 3){
+    if(cont > 4){
         cont = 1 
     }
 
